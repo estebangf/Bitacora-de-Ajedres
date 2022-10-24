132a94c881a6e71d76ce60b7ea8c5e71261eee12
estebangf <esteban.fernandez93@outlook.com.ar>
2022-10-24 15:48:00 -0300

    Nota diaria iniciada y en proceso

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import ToolBarApp from "../components/toolBarApp/ToolBarApp";
-import AppMenu from "../components/appMenu/AppMenu";
 import Box from "@mui/material/Box";
 import { Paper } from "@mui/material";
 
@@ -13,7 +11,8 @@ function MainLayout() {
         width: "100%",
       }}>
         <Paper sx={{
-          m: 5, p: 5,
+          m: 5, p: 5, pt: 2,
+          position: 'relative'
         }}>
           <Outlet />
         </Paper>
